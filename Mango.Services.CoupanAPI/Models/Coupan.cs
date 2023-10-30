68710415f54df78f8371b9c039c56d3dbbbac95e
SAGaonkar <SAGaonkar@burnsmcd.com>
2023-10-30 10:18:43 +0530

    first commit

---
+++ Mango.Services.CoupanAPI/Models/Coupan.cs
@@ -0,0 +1,13 @@
+﻿namespace Mango.Services.CoupanAPI.Models
+{
+    public class Coupan
+    {
+        public int CoupanId { get; set; }
+
+        public string CoupanCode { get; set; }
+
+        public double DiscountAmount { get; set; }
+
+        public int  MinAmount { get; set; }
+    }
+}
